c831771412680d23fceaecc1175c68b50ec69e83
faun3 <antonioculcea@gmail.com>
2023-09-10 21:50:29 +0300

    fixed font situation (help me)

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 "use client";
+
 export default function Home() {
   const words = "artist,lover,friend,developer,creator,human,dreamer".split(
     ","
   );
   const fonts = "font-saint,font-destra,font-honey".split(",");
   return (
-    <>
+    <main>
       {/* R2L text carousel */}
       {/* credit: https://olavihaapala.fi/2021/02/23/modern-marquee.html */}
       <div
         className="flex whitespace-nowrap overflow-x-scroll motion-safe:overflow-x-hidden relative group"
         aria-hidden="true"
       >
-        <div className="flex motion-safe:animate-marquee group-hover:pause">
+        <div className="fFont creditslex motion-safe:animate-marquee group-hover:pause">
           {words.map((word, idx) => {
             return (
               <span
@@ -41,6 +42,16 @@ export default function Home() {
           })}
         </div>
       </div>
-    </>
+
+      <div>
+        <p>Font credits</p>
+        <div className="flex flex-col gap-4">
+          <span className="font-honey uppercase">Honey</span>
+          <span className="font-saint">Saint</span>
+          <span className="font-destra">Destra</span>
+          <span className="font-bebas">BEBAS NEUE</span>
+        </div>
+      </div>
+    </main>
   );
 }
