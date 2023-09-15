31917bb0fa24f0c35e8315b03025052ea4711bc5
faun3 <antonioculcea@gmail.com>
2023-09-15 11:21:19 +0300

    feat: refine connect component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import About from "@/components/About";
 import { useEffect, useRef } from "react";
 import Lenis from "@studio-freight/lenis";
 import TechStack from "@/components/TechStack";
+import Connect from "@/components/Connect";
 
 export default function Home() {
   useEffect(() => {
@@ -25,24 +26,12 @@ export default function Home() {
 
   return (
     <>
-      {/* <div className="">
-        <div className="relative left-[calc(32px+50%)] md:left-[calc(80px+50%)] lg:left-1/2 w-[900px] h-[600px] -translate-x-1/4">
-          {/* image is invalid, the idea sort of sucks */}
-      {/* new idea: make a pixel spray thing that your text can loop over */}
-      {/* the best color for it would probably be gray */}
-      {/* position it offset so it peeks from the right side (just like this stupid image idea) */}
-      {/* <Image */}
-      {/* src={"/"} */}
-      {/* fill={true} */}
-      {/* alt="creek in a forest"></Image> */}
-      {/* </div> */}
-      {/* </div> */}
-
       <TextMarquee />
       <main className="md:w-[864px] lg:w-[1184px] w-[85%] mx-auto">
         <Mission />
         <About />
         <TechStack />
+        <Connect />
         <p>Font credits</p>
         <div className="flex flex-col gap-4">
           <span className="font-honey uppercase">Honey</span>
--- components/Connect.tsx
+++ components/Connect.tsx
@@ -1,13 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import githubIcon from "@/public/icons/githubIcon.svg";
+import linkedInIcon from "@/public/icons/linkedInIcon.svg";
+
 const Connect = () => {
   return (
     <>
-      <div className="mb-[8rem]">
-        <div className="rounded-full aspect-square w-[50%]">
-          <p>say hi</p>
+      <p className="font-bebas text-[4rem] text-center mb-[4rem]">
+        Let&apos;s talk
+      </p>
+      <div className="mb-[8rem] bg-[#aba0a0] rounded-[20px] w-fit p-[4rem] px-[6rem] mx-auto">
+        <div className="rounded-full aspect-square w-[100%] flex justify-center items-center bg-[#eee8e8] mx-auto mb-[4rem]">
+          <Link
+            href={"#"}
+            className="body-2">
+            say hi :&#41;
+          </Link>
         </div>
-        <p>myemailhere@gmail.com</p>
-        <div className="flex flex-row flex-wrap">
-          {/* 5 icons with links here */}
+        <p className="mb-[1rem] text-[1.125rem]">myemailhere@gmail.com</p>
+        <div className="flex flex-row gap-[1rem] justify-center">
+          <Link href={"#"}>
+            <Image
+              src={githubIcon}
+              width={48}
+              height={48}
+              alt="github link"></Image>
+          </Link>
         </div>
       </div>
     </>
